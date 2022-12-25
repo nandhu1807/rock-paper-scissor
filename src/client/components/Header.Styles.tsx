d605b3bf6fdeab9d296b82076ce98e2cee4f9bb8
Nandha Kumar S <snandhakumar96@gmail.com>
2022-12-25 13:28:07 +0100

    Nandha: Added Header component to display game title

---
+++ src/client/components/Header.Styles.tsx
@@ -0,0 +1,11 @@
+import Styled from 'styled-components';
+
+export const Body = Styled.div`
+    text-align: center;
+`;
+
+export const Heading = Styled.h1`
+    font-size: calc(1rem + 0.5vw);
+    color: #fff;
+    font-weight: 700;
+`;
---
+++ src/client/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Body, Heading } from './Header.Styles';
+
+function Header() {
+  return (
+    <Body>
+      <Heading>ROCK, PAPER, SCISSORS</Heading>
+    </Body>
+  );
+}
+
+export default Header;
