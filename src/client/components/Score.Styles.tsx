5747603b7214f50de982031d8659bb3499c23d41
Nandha Kumar S <snandhakumar96@gmail.com>
2022-12-25 22:32:42 +0100

    Nandha: Added Component for Displaying score based on the selection

---
+++ src/client/components/Score.Styles.tsx
@@ -0,0 +1,30 @@
+import Styled from 'styled-components';
+
+export const Body = Styled.div`
+    margin-top: 32px;
+    text-align: center;
+`;
+
+export const Container = Styled.div`
+    text-align: center;
+`;
+
+export const ScoreBorder = Styled.div`
+    display: inline-block;
+    background-color: white;
+    border-radius: 5px;
+    padding: 0.5rem 0.5rem;
+    text-align: center;
+    margin-right: 16px;
+`;
+
+export const ScoreHeader = Styled.h3`
+    font-size: 0.5rem;
+    letter-spacing: 0.05rem;
+    color: rgb(42, 70, 192);
+`;
+
+export const ScoreValue = Styled.h1`
+    font-size: 1.75rem;
+    color: rgb(59, 67, 99);
+`;
---
+++ src/client/components/Score.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Body, Container, ScoreBorder, ScoreHeader, ScoreValue } from './Score.Styles';
+
+function Score(props: {
+  gameState: { player1Name: string; player2Name: string; score1: number; score2: number };
+  selectedMode: string;
+}) {
+  return (
+    <>
+      <Body className={'score-body'}>
+        <Container className={'score-container'}>
+          <ScoreBorder className={'score-border-1'}>
+            <ScoreHeader className={'score-header-1'}>{props.gameState.player1Name} SCORE</ScoreHeader>
+            <ScoreValue className={'score-value-1'}>{props.gameState.score1}</ScoreValue>
+          </ScoreBorder>
+          {props.selectedMode !== 'P VS C' && (
+            <ScoreBorder className={'score-border-2'}>
+              <ScoreHeader className={'score-header-2'}>{props.gameState.player2Name} SCORE</ScoreHeader>
+              <ScoreValue className={'score-value-2'}>{props.gameState.score2}</ScoreValue>
+            </ScoreBorder>
+          )}
+        </Container>
+      </Body>
+    </>
+  );
+}
+
+Score.propTypes = {
+  gameState: PropTypes.object,
+  selectedMode: PropTypes.string,
+};
+
+export default Score;
